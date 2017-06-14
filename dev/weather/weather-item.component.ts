ed958c48b2a5d0c7aa9b3ee91df1ee3e4dd90a73
camblrt <camille.bleriot@cpe.fr>
2017-06-15 00:04:58 +0200

    Add files + weather presentation

--- dev/weather/weather-item.component.ts
+++ dev/weather/weather-item.component.ts
@@ -7,34 +7,45 @@ import {WeatherItem} from "./weather-item"
     <div class="panel panel-danger">
       <div class="panel-heading">{{weatherItem.cityName}}</div>
         <ul class="list-group">
-          <div class="col-md-offset-1">
-            <ul class="calendar">
-              <li class="calendar__item">
+          <div align="center">
+            <h3>TODAY</h3>
                 <div class="temp">{{weatherItem.temperature0}}°{{weatherItem.unit}}<span class="icon-brightness-half"></span></div>
                 <div class="temp">{{weatherItem.description0}}</div>
                 <img src="http://openweathermap.org/themes/openweathermap/assets/vendor/owm/img/widgets/{{weatherItem.icon0}}.png" width="100">
-              </li>
-              <li class="calendar__item">
-                <div class="temp">{{weatherItem.temperature1}}°{{weatherItem.unit}}<span class="icon-brightness-half"></span></div>
-                <div class="temp">{{weatherItem.description1}}</div>
-                <img src="http://openweathermap.org/themes/openweathermap/assets/vendor/owm/img/widgets/{{weatherItem.icon1}}.png" width="100">
-              </li>
-              <li class="calendar__item">
-                <div class="temp">{{weatherItem.temperature2}}°{{weatherItem.unit}}<span class="icon-brightness-half"></span></div>
-                <div class="temp">{{weatherItem.description2}}</div>
-                <img src="http://openweathermap.org/themes/openweathermap/assets/vendor/owm/img/widgets/{{weatherItem.icon2}}.png" width="100">
-              </li>
-              <li class="calendar__item">
-                <div class="temp">{{weatherItem.temperature3}}°{{weatherItem.unit}}<span class="icon-brightness-half"></span></div>
-                <div class="temp">{{weatherItem.description3}}</div>
-                <img src="http://openweathermap.org/themes/openweathermap/assets/vendor/owm/img/widgets/{{weatherItem.icon3}}.png" width="100">
-              </li>
-              <li class="calendar__item">
-                <div class="temp">{{weatherItem.temperature4}}°{{weatherItem.unit}}<span class="icon-brightness-half"></span></div>
-                <div class="temp">{{weatherItem.description4}}</div>
-                <img src="http://openweathermap.org/themes/openweathermap/assets/vendor/owm/img/widgets/{{weatherItem.icon4}}.png" width="100">
-              </li>
-            </ul>
+          </div>
+
+
+
+
+          <div class="row">
+            <div align="center">
+              <tr>
+                <td>
+                  <h5>TOMORROW / </h5>
+                  <div>{{weatherItem.temperature1}}°{{weatherItem.unit}}<span class="icon-brightness-half"></span></div>
+                  <div>{{weatherItem.description1}}</div>
+                  <img src="http://openweathermap.org/themes/openweathermap/assets/vendor/owm/img/widgets/{{weatherItem.icon1}}.png" width="100">
+                </td>
+                <td>
+                  <h5>IN TWO DAYS / </h5>
+                  <div>{{weatherItem.temperature2}}°{{weatherItem.unit}}<span class="icon-brightness-half"></span></div>
+                  <div>{{weatherItem.description2}}</div>
+                  <img src="http://openweathermap.org/themes/openweathermap/assets/vendor/owm/img/widgets/{{weatherItem.icon2}}.png" width="100">
+                </td>
+                <td>
+                  <h5>IN THREE DAYS / </h5>
+                  <div>{{weatherItem.temperature3}}°{{weatherItem.unit}}<span class="icon-brightness-half"></span></div>
+                  <div>{{weatherItem.description3}}</div>
+                  <img src="http://openweathermap.org/themes/openweathermap/assets/vendor/owm/img/widgets/{{weatherItem.icon3}}.png" width="100">
+                </td>
+                <td>
+                  <h5>IN FOUR DAYS</h5>
+                  <div>{{weatherItem.temperature4}}°{{weatherItem.unit}}<span class="icon-brightness-half"></span></div>
+                  <div>{{weatherItem.description4}}</div>
+                  <img src="http://openweathermap.org/themes/openweathermap/assets/vendor/owm/img/widgets/{{weatherItem.icon4}}.png" width="100">
+                </td>
+              </tr>
+            </div>
           </div>
         </ul>
       </div>
